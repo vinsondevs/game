561c4fd21c0de204d4f4fe049d04cf399d68a6a4
vinsondevs <40693206+vinsondevs@users.noreply.github.com>
2018-07-06 12:49:02 +0530

    Update keys.js
    Username and password were hided

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI : 'mongodb://vinson:vinson123@ds127771.mlab.com:27771/students_game'
+    mongoURI : 'mongodb://userName:Password@ds127771.mlab.com:27771/students_game' //replace with your username and password
 }
